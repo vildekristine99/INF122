3764cdd0605434bbde978f06c7d673a4d77afb99
vildekristine99 <578051@stud.hvl.no>
2020-09-03 16:17:51 +0200

    Oppdatering

--- Ukesoppgaver/uke3.hs
+++ Ukesoppgaver/uke3.hs
@@ -5,8 +5,8 @@ sant :: Bool -> Bool -> Bool
 sant x y = if (x == True) then 
                 if y == True then True else False 
             else False
---eller
---sant x y = if (x==True && y==True) then True else False
+{-eller
+sant x y = if (x==True && y==True) then True else False-}
 
 --4.7
 mult :: Int -> Int -> Int -> Int
@@ -26,7 +26,19 @@ perfects n = [x | x <- [1..n], x == sum (factors x)]
 
 --5.7
 --[(x,y) | x <- [1,2], y <- [3,4]]
-concat :: [[a]] -> [a]
-concat = [] 
+fem :: [(Int,Int)]
+fem = concat [ [(x,y) | y <- [3,4]] | x <- [1,2] ]
 
 --5.9
+scalarproduct :: [Int] -> [Int] -> Int
+scalarproduct xs ys = sum [(x*y) | (x,y) <- zip xs ys]
+
+
+--C
+--rem1 :: Eq a => [a] -> a -> [a]
+--rem1 xs x = if (head xs == x) then tail xs else rem  
+--[ xs <- drop 1 xs | head xs == x]
+
+--D
+diff :: Eq a => [a] -> [a] -> [a]
+diff xs ys = []
